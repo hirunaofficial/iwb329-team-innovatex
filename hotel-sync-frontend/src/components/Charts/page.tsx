216b7645efa06d77708bc7980695002348270669
GD Hiruna <hirunaofficial@gmail.com>
2024-10-09 19:50:42 +0530

    Updated Chart component to display BookingSummary, RevenueDetails, and ServiceRequestDetails components

--- hotel-sync-frontend/src/components/Charts/page.tsx
+++ hotel-sync-frontend/src/components/Charts/page.tsx
@@ -1,23 +1,22 @@
-"use client";
-import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import ChartOne from "@/components/Charts/ChartOne";
-import ChartTwo from "@/components/Charts/ChartTwo";
-import dynamic from "next/dynamic";
+'use client';
+import BookingSummary from "@/components/Charts/BookingSummary";
+import RevenueDetails from "@/components/Charts/RevenueDetails";
+import ServiceRequestDetails from "@/components/Charts/ServiceRequestDetails";
 import React from "react";
-
-const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
-  ssr: false,
-});
 
 const Chart: React.FC = () => {
   return (
     <>
-      <Breadcrumb pageName="Chart" />
-
-      <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
+      <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5 mt-10">
+        <div className="col-span-12">
+          <BookingSummary />
+        </div>
+        <div className="col-span-12 xl:col-span-6">
+          <RevenueDetails />
+        </div>
+        <div className="col-span-12 xl:col-span-6">
+          <ServiceRequestDetails />
+        </div>
       </div>
     </>
   );
